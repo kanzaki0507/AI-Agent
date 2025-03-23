4f092fffe9224b146e1bfbd46b07038ec931780c
kanzaki0507 <kanzaki.takemotolab@gmail.com>
2025-03-23 13:03:19 +0900

    init

---
+++ chapter09/tools/search_ddg.py
@@ -0,0 +1,38 @@
+from itertools import islice
+from duckduckgo_search import DDGS
+from langchain_core.tools import tool
+from langchain_core.pydantic_v1 import (BaseModel, Field)
+
+class SearchDDGInput(BaseModel):
+    query: str = Field(description="検索したいキーワードを入力して下さい")
+
+@tool(args_schema=SearchDDGInput)
+def search_ddg(query, max_result_num=5):
+    """
+    DuckDuckGo検索を実行するためのツールです。
+    検索したいキーワードを入力して使用して下さい。
+    検索結果の各ページのタイトル、スニペット（説明文）、URLが返されます。
+    このツールから得られる説明文は非常に簡潔で、時には古い情報の場合もあります。
+
+    必要な情報が見つからない場合は、必ず `fetch_page` ツールを使用して各ページの内容を確認して下さい。
+    文脈に応じて最も適切な言語を使用して下さい（ユーザーの言語と同じである必要はありません）。
+    例えば、プログラミング関連の質問では、英語で検索するのが最適です。
+
+    Returns
+    -------
+    List[Dict[str, str]]:
+    - title
+    - snippet
+    - url
+    """
+
+    res  =DDGS().text(query, region='wt-wt', safesearch='off', backend="lite")
+
+    return [
+        {
+            "title": r.get('title', ""),
+            "snippet": r.get('body', ""),
+            "url": r.get('href', "")
+        }
+        for r in islice(res, max_result_num)
+    ]
